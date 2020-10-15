be0957be362a3246d6880aeed7c75b48860441cc
Back-Log <pratyushnasa@gmail.com>
2020-10-15 12:57:10 +0530

    Bash helloworld

---
+++ hello.sh
@@ -0,0 +1,13 @@
+# first we will write the directory of bash shell
+# ! /bib/bash
+# used for comment
+echo "Hello Word" # This is comment
+#let's practice some System variable
+echo $BASH #gives the directory where the bash is located
+echo $BASH_VERSION #this gives the version of bash
+echo $HOME #give you home directory
+echo $PWD #gives present working directory
+# let's use some user defiend variable
+name=Pratyush
+number=50
+echo My name is $name and My Number is $number
